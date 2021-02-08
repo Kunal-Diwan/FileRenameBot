f5df155cdcb2ec5f0447301c8d43d79e8849a98d
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-02-08 15:44:57 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -4,18 +4,18 @@ from telethon.errors.rpcerrorlist import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
 
 async def check_user(id):
-    if CHANNEL is None:     
+    if AUTH_CHANNEL is None:     
         return True
     ok = True
     try:
-        await TelegramClient(GetParticipantRequest(channel=CHANNEL, user_id=id))
+        await TelegramClient(GetParticipantRequest(channel=AUTH_CHANNEL, user_id=id))
         ok = True
     except UserNotParticipantError:
         ok = False
     return ok
 
 if CHANNEL.startswith('@'):
-    x = CHANNEL.split('@')[1]
+    x = AUTH_CHANNEL.split('@')[1]
     Mai_bOTs = f"https://t.me/{x}"
 else:
-    Mai_bOTs = CHANNEL
+    Mai_bOTs = AUTH_CHANNEL
