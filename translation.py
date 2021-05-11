57fba9275c3bd98e6f6b821a3962c6f80ef94a81
DevelopedBots <83950041+DevelopedBots@users.noreply.github.com>
2021-05-11 07:38:46 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -7,7 +7,7 @@ class Translation(object):
 """
 
     RENAME_403_ERR = "What Are You Doing? My Fool!!"
-    BANNED_USER_TEXT = "Sorry!! But Mai Owner As Banned You From Using This Service. That Means You Can't Run Meh Now! \n \n Contact : @No_OnE_Kn0wS_Me For more Details.. " 
+    BANNED_USER_TEXT = "Sorry!! But Mai Owner As Banned You From Using This Service. That Means You Can't Run Meh Now! \n \n Contact : @DevelopedBotz For more Details.. " 
     ABS_TEXT = "What Are You Trying To Do,Mate?"
     UPGRADE_TEXT = "CONTACT @DevelopedBots "
     DOWNLOAD_START = "<b>Downloading To My server !! pls Wait</b>"
@@ -28,12 +28,12 @@ class Translation(object):
     NO_THUMB_FOUND = "No Thumbnail found"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
-    ABOUT_ME = """<b>Meh : \n An Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Custom Thumbs All The Time.\n \n i can also convert files into videos \n Feedback @No_OnE_Kn0wS_Me If You Found Meh Useful</b>"""
+    ABOUT_ME = """<b>ABOUT ME : \n AM AN TELEGRAM SIMPLE FILE RENAMER BOT. \n YOU CAN RENAME TELEGRAM FILES WITH PERMANANT THUMBNAIL SUPPORT ON ME.\n \n ALSO CONVERT FILES TO VIDEOS \n Feedback DEVOLEPEDBOTZ If You Found ME Useful</b>"""
     HELP_USER = """Ohh You Want Help?? 😅
     
 1.🔹 <b>Send me any Telegram File.</b> \n
 2.🔹 <b>Reply to that message with /rename New file name.extnsion</b> \n
 3.🔹 <b> Send A Photo to make it as permanent Thumbnail</b> \n
 4.🔹 <b> I Can Also Convert Files Into Video through /c2v Command </b>   \n
-5.🔹 <b> Send Me Any Zee5 Url And I'll Upload It Into Telegram </b> \n \n <b>Made With Love By: @MaI_BoTs </b>"""
+5.🔹 <b> Send Me Any Zee5 Url And I'll Upload It Into Telegram </b> \n \n <b>Made With Love By: @DevelopedBotz </b>"""
 
