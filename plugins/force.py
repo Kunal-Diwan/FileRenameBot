8c1539bbc0f3048b1f0409d103e3f9292aa77338
DevelopedBots <83950041+DevelopedBots@users.noreply.github.com>
2021-05-13 17:04:03 +0530

    Update force.py

--- plugins/force.py
+++ plugins/force.py
@@ -53,12 +53,12 @@ async def text(bot, update):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
+                    InlineKeyboardButton('Channel 🔔', url='https://t.me/DevelopedBots'),
+                    InlineKeyboardButton('Support 📢', url='https://t.me/DevelopedBotz')
                 ],
                 [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
+                    InlineKeyboardButton('Source 🖥', url='https://github.com/DevelopedBots/FileRenameBot'),
+                    InlineKeyboardButton('Donate 💸', url='https://www.paypal.me/kunaldiwan1')
                 ]
             ]
         ),
