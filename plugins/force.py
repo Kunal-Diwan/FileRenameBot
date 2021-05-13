1a4a0b0fe8d438ef7fa38b0ed60d183b255bfae5
DevelopedBots <83950041+DevelopedBots@users.noreply.github.com>
2021-05-13 10:02:43 +0530

    Update force.py

--- plugins/force.py
+++ plugins/force.py
@@ -56,6 +56,7 @@ async def text(bot, update):
                     InlineKeyboardButton('Updates 🔔', url='https://t.me/DevelopedBots'),
                     InlineKeyboardButton('Support 📢', url='https://t.me/DevelopedBotz')
                 ],
+                [InlineKeyboardButton(text="🔰MAKE BOT LIKE ME🔰", url="https://youtu.be/q92-LTGwn9k")], 
                 [
                     InlineKeyboardButton('Source 🖥', url='https://github.com/DevelopedBots/FileRenameBot'),
                     InlineKeyboardButton('Donate 💸', url='http://www.paypal.me/kunaldiwan1')
