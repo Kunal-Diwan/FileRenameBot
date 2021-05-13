394aaddc61a1d0ec88ef5a43444bda0d57a6521b
DevelopedBots <83950041+DevelopedBots@users.noreply.github.com>
2021-05-13 17:08:07 +0530

    Update force.py

--- plugins/force.py
+++ plugins/force.py
@@ -42,9 +42,9 @@ async def text(bot, update):
         except UserNotParticipant:
             #await update.reply_text(f"Join @{update_channel} To Use Me")
             await update.reply_text(
-                text="**Please Join My Update Channel Before Using Me..**",
+                text="**📲 Please Join My Update Channel Before Using Me 📲**",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+                    [ InlineKeyboardButton(text="↗️ Join My Updates Channel ↗️", url=f"https://t.me/{update_channel}")]
               ])
             )
             return
